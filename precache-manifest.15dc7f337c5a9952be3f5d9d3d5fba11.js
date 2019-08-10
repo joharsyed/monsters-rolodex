4f73f0d4bbb6bb0870c0b391fc8c01ffd449052d
Johar <49555816+joharsyed@users.noreply.github.com>
2019-08-10 10:42:37 +0200

    Updates

---
+++ precache-manifest.15dc7f337c5a9952be3f5d9d3d5fba11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0efc14beaf14c9388e789dd47e668a1d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a863474d558e3b1e90f7",
+    "url": "/monsters-rolodex/static/css/main.94c793f2.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "a863474d558e3b1e90f7",
+    "url": "/monsters-rolodex/static/js/main.7c4db17e.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
